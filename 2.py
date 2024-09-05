1800dd5227317977f4362dcdf850837b7e0305d8
Juúnior Santos <junio2063@gmail.com>
2024-09-05 15:08:17 -0300

    Upando questões

---
+++ 2.py
@@ -0,0 +1,9 @@
+def verificar_letra_a(string):
+    count_a = string.lower().count('a')
+    if count_a > 0:
+        print(f"A letra 'a' aparece {count_a} vez(es) na string.")
+    else:
+        print("A letra 'a' não aparece na string.")
+ 
+string = input("Informe uma string: ")
+verificar_letra_a(string)
---
+++ 5.py
@@ -0,0 +1,10 @@
+Vá até a sala dos interruptores:
+Ligue o Interruptor 1 e espere alguns minutos.
+Depois, desligue o Interruptor 1 e ligue o Interruptor 2.
+Deixe o Interruptor 3 desligado o tempo todo.
+
+Agora, vá para a sala das lâmpadas:
+A lâmpada que estiver acesa é controlada pelo Interruptor 2 (que ficou ligado).
+A lâmpada que estiver desligada, mas quente é controlada pelo Interruptor 1 (que foi ligado e depois desligado).
+A lâmpada que estiver desligada e fria é controlada pelo Interruptor 3 (que nunca foi ligado).
+Assim, com apenas duas idas, você descobre qual interruptor controla cada lâmpada!
